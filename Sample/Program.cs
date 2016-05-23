813ae3303b6754cd715650acebb910a47cbbf262
Uffe Björklund <uffe@xsockets.net>
2016-05-23 15:17:47 +0200

    Updated to XSockets 5

---
+++ Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using XSockets.Core.Common.Socket;
+using XSockets.Plugin.Framework;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var container = Composable.GetExport<IXSocketServerContainer>())
+            {
+                container.Start();
+                Console.ReadLine();
+                container.Stop();
+            }
+        }
+    }
+}
