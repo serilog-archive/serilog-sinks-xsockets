7183d0608b095053b86c8025a1cb7f2874921b5d
Uffe Björklund <uffe@xsockets.net>
2016-05-23 15:25:18 +0200

    ...

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,6 +4,10 @@ using XSockets.Plugin.Framework;
 
 namespace Sample
 {
+    /// <summary>
+    /// Since XSockets use Serilog the serilog XSockets sink will work directly.
+    /// See - https://github.com/serilog/serilog-sinks-xsockets
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
